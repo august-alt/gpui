a1ace64d159d8a3350667a8107d77c42c75868ba
august-alt <77973983+august-alt@users.noreply.github.com>
2021-07-19 17:22:30 +0400

    feat: add io for registry files

---
+++ src/io/registryfile.h
@@ -0,0 +1,58 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef GPUI_RegistryFile_H
+#define GPUI_RegistryFile_H
+
+#include "io.h"
+
+#include "genericfile.h"
+#include "registryfileformat.h"
+
+#include <memory>
+
+namespace model {
+    namespace registry {
+        class Registry;
+    }
+}
+
+namespace io {
+    class RegistryFilePrivate;
+
+    class GPUI_IO_EXPORT RegistryFile : public GenericFile<RegistryFile, RegistryFileFormat<RegistryFile>>
+    {
+    public:
+        RegistryFile();
+
+        ~RegistryFile();
+
+        void addRegistryEntry(std::shared_ptr<model::registry::Registry> registryEntry);
+
+        void removeRegistryEntry(std::shared_ptr<model::registry::Registry> registryEntry);
+
+        std::shared_ptr<model::registry::Registry> getRegistry();
+
+    private:
+        RegistryFilePrivate* const d;
+    };
+}
+
+#endif // GPUI_RegistryFile_H
---
+++ src/io/registryfileformat.h
@@ -0,0 +1,63 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef GPUI_REGISTRYFILEFORMAT_H
+#define GPUI_REGISTRYFILEFORMAT_H
+
+#include "io.h"
+
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace io {
+    class RegistryFileFormatPrivate;
+
+    template <typename TRegistryFile>
+    class GPUI_IO_EXPORT RegistryFileFormat
+    {
+    public:
+        std::string getName();
+
+        virtual bool read(std::istream& input, TRegistryFile* file) = 0;
+
+        virtual bool write(std::ostream& output, TRegistryFile* file) = 0;
+
+        std::string getErrorString();
+
+        static RegistryFileFormat* create(const std::string& name);
+
+        static std::vector<std::string> getFormats();
+
+        virtual ~RegistryFileFormat();
+
+    protected:
+        void setErrorString(const std::string& error);
+
+        RegistryFileFormat(const std::string& name);
+
+    private:
+        RegistryFileFormatPrivate* const d;
+    };
+}
+
+#include "registryfileformat.inl"
+
+#endif // GPUI_REGISTRYFILEFORMAT_H

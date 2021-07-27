28d4e70f27e0a4b3a67ab5036a6ef64cefcc970c
august-alt <77973983+august-alt@users.noreply.github.com>
2021-07-27 16:23:47 +0400

    feat: add main window settings class

---
+++ src/gui/mainwindowsettings.h
@@ -0,0 +1,53 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef GPUI_MAIN_WINDOW_SETTINGS_H
+#define GPUI_MAIN_WINDOW_SETTINGS_H
+
+#include <QObject>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+namespace gpui {
+
+class MainWindow;
+class MainWindowSettingsPrivate;
+
+class MainWindowSettings : public QObject
+{
+    Q_OBJECT
+public:
+    MainWindowSettings(MainWindow *window, Ui::MainWindow *ui);
+    ~MainWindowSettings();
+
+    void restoreSettings();
+
+public slots:
+    void saveSettings();
+
+private:
+    MainWindowSettingsPrivate* d;
+};
+
+}
+
+#endif // GPUI_MAIN_WINDOW_SETTINGS_H

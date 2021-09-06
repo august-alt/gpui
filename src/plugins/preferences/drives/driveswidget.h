e2611c1701199f6916ffee831c120b44163c42b2
august-alt <77973983+august-alt@users.noreply.github.com>
2021-09-06 19:08:40 +0400

    wip, feat: add preferences widgets

---
+++ src/plugins/preferences/drives/driveswidget.h
@@ -0,0 +1,45 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef GPUI_DRIVESWIDGET_H
+#define GPUI_DRIVESWIDGET_H
+
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class DrivesWidget; }
+QT_END_NAMESPACE
+
+namespace gpui
+{
+    class DrivesWidget : public QWidget
+    {
+        Q_OBJECT
+
+    public:
+        DrivesWidget(QWidget *parent = 0);
+        ~DrivesWidget();
+
+    private:
+        Ui::DrivesWidget *ui;
+    };
+}
+
+#endif // GPUI_DRIVESWIDGET_H

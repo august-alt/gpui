3c94157fd511779635f7f009bbfadea141717c38
august-alt <77973983+august-alt@users.noreply.github.com>
2021-09-23 19:30:33 +0400

    feat, build: fix build of gpui

--- script/build.sh
+++ script/build.sh
@@ -5,4 +5,7 @@ mkdir /home/builder2/.hasher
 cp /app/hasher/config /home/builder2/.hasher/config 
 hsh --initroot-only --no-wait-lock -vv /home/builder2/hasher 
 
-cd /app/ && gear-hsh
+hsh-install /home/builder2/hasher /app/RPMS/libnemofolderlistmodel-0.1.0-alt1.x86_64.rpm
+hsh-install /home/builder2/hasher /app/RPMS/libnemofolderlistmodel-devel-0.1.0-alt1.x86_64.rpm
+
+cd /app/ && gear --hasher -- hsh-rebuild /home/builder2/hasher

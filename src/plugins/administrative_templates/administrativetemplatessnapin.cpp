e045273b6ca495602535bd0ffe02e24482431539
august-alt <77973983+august-alt@users.noreply.github.com>
2022-10-05 17:18:20 +0400

    wip: update administrative templates snap-in

--- src/plugins/administrative_templates/administrativetemplatessnapin.cpp
+++ src/plugins/administrative_templates/administrativetemplatessnapin.cpp
@@ -27,14 +27,46 @@
 
 #include "bundle/policybundle.h"
 
+#include "registry/abstractregistrysource.h"
+#include "registry/polregistrysource.h"
+#include "registry/registry.h"
+
+#include "../plugins/storage/smb/smbfile.h"
+
+#include "../io/registryfileformat.h"
+
+#include "../io/genericreader.h"
+#include "../io/registryfile.h"
+#include "../io/registryfileformat.h"
+
+#include "../io/policydefinitionsfile.h"
+#include "../io/policyfileformat.h"
+
+#include "../io/inifile.h"
+
+#include <QDebug>
+
 namespace gpui
 {
 class AdministrativeTemplatesSnapInPrivate
 {
 public:
-    AdministrativeTemplatesSnapInPrivate() {}
-
     std::unique_ptr<QStandardItemModel> model = nullptr;
+
+    std::shared_ptr<model::registry::Registry> userRegistry{};
+    std::unique_ptr<model::registry::AbstractRegistrySource> userRegistrySource{};
+    QString userRegistryPath{};
+
+    std::shared_ptr<model::registry::Registry> machineRegistry{};
+    std::unique_ptr<model::registry::AbstractRegistrySource> machineRegistrySource{};
+    QString machineRegistryPath{};
+
+    AdministrativeTemplatesSnapInPrivate()
+        : userRegistry(new model::registry::Registry())
+        , userRegistrySource(new model::registry::PolRegistrySource(userRegistry))
+        , machineRegistry(new model::registry::Registry())
+        , machineRegistrySource(new model::registry::PolRegistrySource(machineRegistry))
+    {}
 
 private:
     AdministrativeTemplatesSnapInPrivate(const AdministrativeTemplatesSnapInPrivate &) = delete; // copy ctor
@@ -43,6 +75,57 @@ private:
         = delete; // copy assignment
     AdministrativeTemplatesSnapInPrivate &operator=(AdministrativeTemplatesSnapInPrivate &&) = delete; // move assignment
 };
+
+void onPolFileOpen(const QString &path,
+                   std::shared_ptr<model::registry::Registry> &registry,
+                   std::unique_ptr<model::registry::AbstractRegistrySource> &source,
+                   std::function<void(model::registry::AbstractRegistrySource *)> callback)
+{
+    qWarning() << "Path recieved: " << path;
+
+    auto stringvalues = std::make_unique<std::string>();
+
+    try
+    {
+        if (path.startsWith("smb://"))
+        {
+            gpui::smb::SmbFile smbLocationItemFile(path);
+            smbLocationItemFile.open(QFile::ReadOnly);
+            stringvalues->resize(smbLocationItemFile.size(), 0);
+            smbLocationItemFile.read(&stringvalues->at(0), smbLocationItemFile.size());
+            smbLocationItemFile.close();
+        }
+        else
+        {
+            QFile registryFile(path);
+            registryFile.open(QFile::ReadWrite);
+            stringvalues->resize(registryFile.size(), 0);
+            registryFile.read(&stringvalues->at(0), registryFile.size());
+            registryFile.close();
+        }
+
+        auto iss = std::make_unique<std::istringstream>(*stringvalues);
+        std::string pluginName("pol");
+
+        auto reader       = std::make_unique<io::GenericReader>();
+        auto registryFile = reader->load<io::RegistryFile, io::RegistryFileFormat<io::RegistryFile>>(*iss, pluginName);
+        if (!registryFile)
+        {
+            qWarning() << "Unable to load registry file contents.";
+            return;
+        }
+
+        registry = registryFile->getRegistry();
+
+        source = std::make_unique<model::registry::PolRegistrySource>(registry);
+
+        callback(source.get());
+    }
+    catch (std::exception &e)
+    {
+        qWarning() << "Warning: Unable to read file: " << qPrintable(path) << " description: " << e.what();
+    }
+}
 
 AdministrativeTemplatesSnapIn::AdministrativeTemplatesSnapIn()
     : AbstractSnapIn("ISnapIn",
@@ -57,9 +140,7 @@ AdministrativeTemplatesSnapIn::AdministrativeTemplatesSnapIn()
 void AdministrativeTemplatesSnapIn::onInitialize()
 {
     auto bundle = std::make_unique<model::bundle::PolicyBundle>();
-    d->model    = bundle->loadFolder("/home/august/Downloads/ADMX/Program Files/Microsoft Group "
-                                  "Policy/Windows 10 October 2020 Update (20H2)/PolicyDefinitions/",
-                                  "ru-ru");
+    d->model    = bundle->loadFolder("/usr/share/PolicyDefinitions/", "ru-ru");
 
     setRootNode(static_cast<QAbstractItemModel *>(d->model.get()));
 

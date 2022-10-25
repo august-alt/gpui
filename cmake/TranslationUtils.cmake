e14d73416b1b338154be49695e4981520d8bbe21
august-alt <77973983+august-alt@users.noreply.github.com>
2022-10-25 18:32:40 +0400

    feat: implement translation in preferences snap-in

--- cmake/TranslationUtils.cmake
+++ cmake/TranslationUtils.cmake
@@ -11,3 +11,21 @@ function(add_translation_resource out_file)
 
     set(${out_file} ${resource} PARENT_SCOPE)
 endfunction()
+
+function(add_translation out_files)
+    set(qm_files "")
+    foreach(filename ${ARGN})
+        get_filename_component(basename ${filename} NAME_WE)
+        set(qm "${CMAKE_CURRENT_SOURCE_DIR}/${basename}.qm")
+        message("${qm}")
+        add_custom_command(
+            OUTPUT "${qm}"
+            COMMAND "${Qt5_LRELEASE_EXECUTABLE}"
+            ARGS -markuntranslated "Is not translated!" -nounfinished -removeidentical -compress "${filename}" -qm "${qm}"
+            DEPENDS "${filename}" VERBATIM
+        )
+        list(APPEND qm_files "${qm}")
+    endforeach()
+
+    set(${out_files} ${qm_files} PARENT_SCOPE)
+endfunction()

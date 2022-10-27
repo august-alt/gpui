1d3cd8fb09ba3122f863409f34a9bb817e7fc281
august-alt <77973983+august-alt@users.noreply.github.com>
2022-10-27 16:35:04 +0400

    feat: add administrative templates widget

---
+++ src/plugins/administrative_templates/ui/administrativetemplateswidget.h
@@ -0,0 +1,58 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2022 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef GPUI_ADMINISTRATIVE_TEMPLATES_WIDGET_H
+#define GPUI_ADMINISTRATIVE_TEMPLATES_WIDGET_H
+
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+namespace Ui
+{
+class AdministrativeTemplatesWidget;
+}
+QT_END_NAMESPACE
+
+class QDataWidgetMapper;
+
+namespace gpui
+{
+class AdministrativeTemplatesWidget : public QWidget
+{
+public:
+    Q_OBJECT
+
+public:
+    explicit AdministrativeTemplatesWidget(QWidget *parent = nullptr);
+    ~AdministrativeTemplatesWidget();
+
+private:
+    AdministrativeTemplatesWidget(const AdministrativeTemplatesWidget &) = delete;            // copy ctor
+    AdministrativeTemplatesWidget(AdministrativeTemplatesWidget &&)      = delete;            // move ctor
+    AdministrativeTemplatesWidget &operator=(const AdministrativeTemplatesWidget &) = delete; // copy assignment
+    AdministrativeTemplatesWidget &operator=(AdministrativeTemplatesWidget &&) = delete;      // move assignment
+
+private:
+    Ui::AdministrativeTemplatesWidget *ui{nullptr};
+};
+
+} // namespace gpui
+
+#endif //GPUI_ADMINISTRATIVE_TEMPLATES_WIDGET_H

26bde93a05c3c44411042987ca7241d6907b1f47
august-alt <77973983+august-alt@users.noreply.github.com>
2022-10-31 15:20:00 +0400

    feat: implement translation for administrative templates

---
+++ src/plugins/administrative_templates/ui/i18n/common_translation_en.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>AdministrativeTemplatesWidget</name>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="106"/>
+        <source>Policy State</source>
+        <translation>Policy State</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="115"/>
+        <source>Not Configured</source>
+        <translation>Not Configured</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="122"/>
+        <source>Enabled</source>
+        <translation>Enabled</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="129"/>
+        <source>Disabled</source>
+        <translation>Disabled</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="184"/>
+        <source>Supported on:</source>
+        <translation>Supported on:</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="213"/>
+        <source>Help:</source>
+        <translation>Help:</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="283"/>
+        <source>&amp;Cancel</source>
+        <translation>&amp;Cancel</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.ui" line="290"/>
+        <source>&amp;OK</source>
+        <translation>&amp;OK</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../administrativetemplatessnapin.cpp" line="87"/>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <location filename="../../administrativetemplatessnapin.cpp" line="88"/>
+        <source>Error writing file:</source>
+        <translation>Error writing file:</translation>
+    </message>
+    <message>
+        <location filename="../../bundle/policybundle.cpp" line="90"/>
+        <source>[Local Group Policy]</source>
+        <translation>[Local Group Policy]</translation>
+    </message>
+    <message>
+        <location filename="../../bundle/policybundle.cpp" line="92"/>
+        <source>Local group policies</source>
+        <translation>Local group policies</translation>
+    </message>
+    <message>
+        <location filename="../../bundle/policybundle.cpp" line="98"/>
+        <source>Machine</source>
+        <translation>Machine</translation>
+    </message>
+    <message>
+        <location filename="../../bundle/policybundle.cpp" line="100"/>
+        <source>Machine level policies</source>
+        <translation>Machine level policies</translation>
+    </message>
+    <message>
+        <location filename="../../bundle/policybundle.cpp" line="103"/>
+        <source>User</source>
+        <translation>User</translation>
+    </message>
+    <message>
+        <location filename="../../bundle/policybundle.cpp" line="105"/>
+        <source>User level policies</source>
+        <translation>User level policies</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.cpp" line="247"/>
+        <source>Save settings dialog</source>
+        <translation>Save settings dialog</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.cpp" line="248"/>
+        <source>Policy settings were modified do you want to save them?</source>
+        <translation>Policy settings were modified do you want to save them?</translation>
+    </message>
+</context>
+<context>
+    <name>gpui::AdministrativeTemplatesWidget</name>
+    <message>
+        <location filename="../administrativetemplateswidget.cpp" line="168"/>
+        <source>Policy: </source>
+        <translation>Policy: </translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.cpp" line="251"/>
+        <source>Yes</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <location filename="../administrativetemplateswidget.cpp" line="252"/>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+</context>
+</TS>

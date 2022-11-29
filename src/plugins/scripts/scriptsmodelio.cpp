68ff6ed8fe70185943ee7d673191a5fe33732910
Alexey Saprunov <workernight@yandex.ru>
2022-11-29 19:27:13 +0400

    fix: paths for ini files

--- src/plugins/scripts/scriptsmodelio.cpp
+++ src/plugins/scripts/scriptsmodelio.cpp
@@ -22,10 +22,10 @@ void ScriptsModelIo::loadPolicies(std::string *path,
                                   ScriptsModel *machineScripts,
                                   ScriptsModel *machinePowerScripts)
 {
-    auto machinePathScripts      = *path + "/Machine/scripts.ini";
-    auto machinePathPowerScripts = *path + "/Machine/psscripts.ini";
-    auto userPathScripts         = *path + "/User/scripts.ini";
-    auto userPathPowerScripts    = *path + "/User/psscripts.ini";
+    auto machinePathScripts      = *path + "/Machine/Scripts/scripts.ini";
+    auto machinePathPowerScripts = *path + "/Machine/Scripts/psscripts.ini";
+    auto userPathScripts         = *path + "/User/Scripts/scripts.ini";
+    auto userPathPowerScripts    = *path + "/User/Scripts/psscripts.ini";
 
     loadIniFile(machinePathScripts, machineScripts);
     loadIniFile(machinePathPowerScripts, machinePowerScripts);
@@ -39,10 +39,10 @@ void ScriptsModelIo::savePolicies(std::string *path,
                                   ScriptsModel *machineScripts,
                                   ScriptsModel *machinePowerScripts)
 {
-    auto machinePathScripts      = *path + "/Machine/scripts.ini";
-    auto machinePathPowerScripts = *path + "/Machine/psscripts.ini";
-    auto userPathScripts         = *path + "/User/scripts.ini";
-    auto userPathPowerScripts    = *path + "/User/psscripts.ini";
+    auto machinePathScripts      = *path + "/Machine/Scripts/scripts.ini";
+    auto machinePathPowerScripts = *path + "/Machine/Scripts/psscripts.ini";
+    auto userPathScripts         = *path + "/User/Scripts/scripts.ini";
+    auto userPathPowerScripts    = *path + "/User/Scripts/psscripts.ini";
 
     saveIniFile(machinePathScripts, machineScripts);
     saveIniFile(machinePathPowerScripts, machinePowerScripts);

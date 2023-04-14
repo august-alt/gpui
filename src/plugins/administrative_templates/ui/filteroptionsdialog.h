a5fa56f179502af4e6129a014a9a8cf31ec6f11f
AlexSP0 <76878359+AlexSP0@users.noreply.github.com>
2023-04-14 13:57:54 +0400

    chore: add administrative templates filter dialog and widget

---
+++ src/plugins/administrative_templates/ui/filteroptionsdialog.h
@@ -0,0 +1,54 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2023 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef GPUI_FILTER_OPTIONS_DIALOG_H
+#define GPUI_FILTER_OPTIONS_DIALOG_H
+
+#include <QDialog>
+
+#include "../../../../src/core/common.h"
+
+namespace Ui
+{
+class FilterOptionsDialog;
+}
+
+namespace gpui
+{
+class GPUI_SYMBOL_EXPORT FilterOptionsDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit FilterOptionsDialog(QWidget *parent = nullptr);
+    ~FilterOptionsDialog();
+
+private:
+    Ui::FilterOptionsDialog *ui;
+
+private:
+    FilterOptionsDialog(const FilterOptionsDialog &) = delete;
+    FilterOptionsDialog(FilterOptionsDialog &&)      = delete;
+    FilterOptionsDialog &operator=(const FilterOptionsDialog &) = delete;
+    FilterOptionsDialog &operator=(FilterOptionsDialog &&) = delete;
+};
+} // namespace gpui
+
+#endif // GPUI_FILTER_OPTIONS_DIALOG_H

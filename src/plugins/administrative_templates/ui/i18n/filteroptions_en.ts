e2b50d9db67f00ffdde4949cbe9b8feac9f2915e
Alexey Saprunov <workernight@yandex.ru>
2023-04-14 15:05:12 +0400

    chore: add translations for filteroptionsdialog and filteroptionswidget

---
+++ src/plugins/administrative_templates/ui/i18n/filteroptions_en.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>FilterOptionsDialog</name>
+    <message>
+        <location filename="../filteroptionsdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Dialog</translation>
+    </message>
+</context>
+<context>
+    <name>FilterOptionsWidget</name>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>Form</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="50"/>
+        <source>Select options below to enable and change or disable types of global filters that will be applied to the Administrative Templates nodes.</source>
+        <translation>Select options below to enable and change or disable types of global filters that will be applied to the Administrative Templates nodes.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="91"/>
+        <source>Configured:</source>
+        <translation>Configured:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="101"/>
+        <source>Managed:</source>
+        <translation>Managed:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="127"/>
+        <location filename="../filteroptionswidget.ui" line="161"/>
+        <location filename="../filteroptionswidget.ui" line="202"/>
+        <location filename="../filteroptionswidget.ui" line="281"/>
+        <source>Any</source>
+        <translation>Any</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="132"/>
+        <location filename="../filteroptionswidget.ui" line="166"/>
+        <location filename="../filteroptionswidget.ui" line="207"/>
+        <location filename="../filteroptionswidget.ui" line="286"/>
+        <source>Yes</source>
+        <translation>Yes</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="137"/>
+        <location filename="../filteroptionswidget.ui" line="171"/>
+        <location filename="../filteroptionswidget.ui" line="212"/>
+        <location filename="../filteroptionswidget.ui" line="291"/>
+        <source>No</source>
+        <translation>No</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="179"/>
+        <source>Select the type of policy settings to display.</source>
+        <translation>Select the type of policy settings to display.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="220"/>
+        <source>Commented:</source>
+        <translation>Commented:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="254"/>
+        <source>Enable Keyword Filters</source>
+        <translation>Enable Keyword Filters</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="264"/>
+        <source>Filter for word(s):</source>
+        <translation>Filter for word(s):</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="299"/>
+        <source>Within:</source>
+        <translation>Within:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="327"/>
+        <source>Policy Settings Title</source>
+        <translation>Policy Settings Title</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="343"/>
+        <source>Help Text</source>
+        <translation>Help Text</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="353"/>
+        <source>Comment</source>
+        <translation>Comment</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="387"/>
+        <source>Enable Requirements Filters</source>
+        <translation>Enable Requirements Filters</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="400"/>
+        <location filename="../filteroptionswidget.ui" line="404"/>
+        <source>Include settings that match any of the selected platforms.</source>
+        <translation>Include settings that match any of the selected platforms.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="409"/>
+        <source>Include settings that match all of the selected platforms.</source>
+        <translation>Include settings that match all of the selected platforms.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="423"/>
+        <source>Select the desired platform and application filter(s):</source>
+        <translation>Select the desired platform and application filter(s):</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="459"/>
+        <source>Clear All</source>
+        <translation>Clear All</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="466"/>
+        <source>Select All</source>
+        <translation>Select All</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/administrative_templates/ui/i18n/filteroptions_ru.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>FilterOptionsDialog</name>
+    <message>
+        <location filename="../filteroptionsdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation>Диалог</translation>
+    </message>
+</context>
+<context>
+    <name>FilterOptionsWidget</name>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="14"/>
+        <source>Form</source>
+        <translation>Форма</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="50"/>
+        <source>Select options below to enable and change or disable types of global filters that will be applied to the Administrative Templates nodes.</source>
+        <translation>Выберите опции ниже, чтобы включить, изменить или отключить типы глобальных фильтров, которые будут применяться к узлам административных шаблонов.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="91"/>
+        <source>Configured:</source>
+        <translation>Настроено:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="101"/>
+        <source>Managed:</source>
+        <translation>Установлено:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="127"/>
+        <location filename="../filteroptionswidget.ui" line="161"/>
+        <location filename="../filteroptionswidget.ui" line="202"/>
+        <location filename="../filteroptionswidget.ui" line="281"/>
+        <source>Any</source>
+        <translation>Любой</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="132"/>
+        <location filename="../filteroptionswidget.ui" line="166"/>
+        <location filename="../filteroptionswidget.ui" line="207"/>
+        <location filename="../filteroptionswidget.ui" line="286"/>
+        <source>Yes</source>
+        <translation>Да</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="137"/>
+        <location filename="../filteroptionswidget.ui" line="171"/>
+        <location filename="../filteroptionswidget.ui" line="212"/>
+        <location filename="../filteroptionswidget.ui" line="291"/>
+        <source>No</source>
+        <translation>Нет</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="179"/>
+        <source>Select the type of policy settings to display.</source>
+        <translation>Выберите тип настройки политики для отображения.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="220"/>
+        <source>Commented:</source>
+        <translation>Наличие коментария:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="254"/>
+        <source>Enable Keyword Filters</source>
+        <translation>Включить фильтр по ключевым словам</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="264"/>
+        <source>Filter for word(s):</source>
+        <translation>Фильтр по слову (ам):</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="299"/>
+        <source>Within:</source>
+        <translation>Включая:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="327"/>
+        <source>Policy Settings Title</source>
+        <translation>Название настройки</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="343"/>
+        <source>Help Text</source>
+        <translation>Текст помощи</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="353"/>
+        <source>Comment</source>
+        <translation>Комментарий</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="387"/>
+        <source>Enable Requirements Filters</source>
+        <translation>Включить фильтры требований</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="400"/>
+        <location filename="../filteroptionswidget.ui" line="404"/>
+        <source>Include settings that match any of the selected platforms.</source>
+        <translation>Выбрать политики которые применимы для любой из выбранных платформ.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="409"/>
+        <source>Include settings that match all of the selected platforms.</source>
+        <translation>Выбрать политики которые применимы для всех выбранных платформ.</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="423"/>
+        <source>Select the desired platform and application filter(s):</source>
+        <translation>Выберите желаемые фильтры для платформ и приложений:</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="459"/>
+        <source>Clear All</source>
+        <translation>Очистить все</translation>
+    </message>
+    <message>
+        <location filename="../filteroptionswidget.ui" line="466"/>
+        <source>Select All</source>
+        <translation>Выбрать все</translation>
+    </message>
+</context>
+</TS>

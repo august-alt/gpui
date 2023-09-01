22667dab24dc4d749954a73ae7465307ed4d51cf
august-alt <77973983+august-alt@users.noreply.github.com>
2023-09-01 13:38:17 +0400

    fix: fix issue #47137

--- src/plugins/administrative_templates/registry/policystatemanager.cpp
+++ src/plugins/administrative_templates/registry/policystatemanager.cpp
@@ -220,8 +220,7 @@ void PolicyStateManager::setPolicyStateEnabled()
 
     if (d->policy.valueName.size() > 0
         && !d->policy.enabledValue
-        && d->policy.enabledList.empty()
-        && d->policy.elements.empty())
+        && d->policy.enabledList.empty())
     {
         d->source.setValue(d->policy.key, d->policy.valueName, RegistryEntryType::REG_DWORD, 1);
     }

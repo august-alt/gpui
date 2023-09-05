ecdf98ae9e8e486d091b7a47252247c72ca8c11a
august-alt <77973983+august-alt@users.noreply.github.com>
2023-09-05 15:04:53 +0400

    feat: add administrative templates settings widget

---
+++ src/plugins/administrative_templates/ui/administrativetemplatessettingswidget.cpp
@@ -0,0 +1,29 @@
+#include "administrativetemplatessettingswidget.h"
+#include "ui_administrativetemplatessettingswidget.h"
+
+namespace gpui
+{
+
+AdministrativeTemplatesSettingsWidget::AdministrativeTemplatesSettingsWidget(QWidget *parent)
+    : ISettingsWidget(parent)
+    , ui(new Ui::AdministrativeTemplatesSettingsWidget())
+{
+    ui->setupUi(this);
+}
+
+AdministrativeTemplatesSettingsWidget::~AdministrativeTemplatesSettingsWidget()
+{
+    delete ui;
+}
+
+QString AdministrativeTemplatesSettingsWidget::getName() const
+{
+    return QStringLiteral("Administrative Templates");
+}
+
+void AdministrativeTemplatesSettingsWidget::saveSettings()
+{
+    // TODO: Implement.
+}
+
+}
---
+++ src/plugins/administrative_templates/ui/administrativetemplatessettingswidget.h
@@ -0,0 +1,60 @@
+/***********************************************************************************************************************
+**
+** Copyright (C) 2021 BaseALT Ltd. <org@basealt.ru>
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** as published by the Free Software Foundation; either version 2
+** of the License, or (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+**
+***********************************************************************************************************************/
+
+#ifndef ADMINISTRATIVE_TEMPLATES_SETTINGS_WIDGET_H
+#define ADMINISTRATIVE_TEMPLATES_SETTINGS_WIDGET_H
+
+#include "../../core/isettingswidget.h"
+
+#include <QtWidgets>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class AdministrativeTemplatesSettingsWidget; }
+QT_END_NAMESPACE
+
+namespace gpui
+{
+
+class AdministrativeTemplatesSettingsWidget : public ISettingsWidget
+{
+public:
+    Q_OBJECT
+
+public:
+    explicit AdministrativeTemplatesSettingsWidget(QWidget* parent = nullptr);
+    ~AdministrativeTemplatesSettingsWidget() override;
+
+    QString getName() const override;
+
+    void saveSettings() override;
+
+private:
+    AdministrativeTemplatesSettingsWidget(const AdministrativeTemplatesSettingsWidget&)            = delete;   // copy ctor
+    AdministrativeTemplatesSettingsWidget(AdministrativeTemplatesSettingsWidget&&)                 = delete;   // move ctor
+    AdministrativeTemplatesSettingsWidget& operator=(const AdministrativeTemplatesSettingsWidget&) = delete;   // copy assignment
+    AdministrativeTemplatesSettingsWidget& operator=(AdministrativeTemplatesSettingsWidget&&)      = delete;   // move assignment
+
+private:
+    Ui::AdministrativeTemplatesSettingsWidget *ui {nullptr};
+};
+
+}
+
+#endif//ADMINISTRATIVE_TEMPLATES_SETTINGS_WIDGET_H
